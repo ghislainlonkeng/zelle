46d17a4312177cd923698d82b6ecae2fdaa4f7a7
Ghislain Lonkeng <lonkegghislain@gmail.com>
2022-02-22 03:04:09 +0000

    another update

--- app.java
+++ app.java
@@ -1,1 +1,8 @@
-# this is the java file for our zelle project
+# this is the java file for our zelle projec
+#
+#
+#echo "please enter your username"
+read username
+echo "please enter your password"
+read -s passwrd
+echo "welcome to the zelle application"
